264f9dd026fdb56081e4b476cb7d01c918b2848d
‘chenyuedong’ <10512665545@qq.com>
2023-05-09 11:23:20 +0800

    第一次提交

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_subset(int n) {
+    for (int i = 0; i < (1 << n); ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i & (1 << j))
+                cout << j << " ";
+            cout << endl;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+    print_subset(n);
     return 0;
 }
+
+
